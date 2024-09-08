751724c009f4a357be4e5dfe8bd7c09c99002c49
Bogdan <bohdan@omnifi.io>
2024-09-08 16:12:23 +0300

    minor fix

--- src/db/schemas/Ingredients.ts
+++ src/db/schemas/Ingredients.ts
@@ -5,30 +5,7 @@ const database = getDatabase();
 const collection = database.collection('ingredients');
 
 export const getAllIngredients = async () => {
-  const ingredients = await collection.aggregate<TMongoIngredient>([
-    {
-      $unwind: "$tags"
-    },
-    {
-      $lookup: {
-        from: "ingredient-tags",
-        localField: "tags",
-        foreignField: "id",
-        as: "tagDetails"
-      }
-    },
-    {
-      $unwind: "$tagDetails"
-    },
-    {
-      $group: {
-        _id: "$_id",
-        nameEn: { $first: "$nameEn" },
-        descriptionEn: { $first: "$descriptionEn" },
-        tags: { $push: "$tagDetails.name" }
-      }
-    }
-  ]).toArray();
+  const ingredients = await collection.find().toArray();
 
   return ingredients;
 }
