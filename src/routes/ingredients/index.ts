88965358e17bf680315e424434380f58af65c290
Bogdan <bohdan@omnifi.io>
2024-09-26 15:57:17 +0300

    init auth0

--- src/routes/ingredients/index.ts
+++ src/routes/ingredients/index.ts
@@ -3,11 +3,24 @@ import { getAllIngredients } from '../../db/schemas/Ingredients';
 import { IIngredient } from '../../types/ingredient';
 import { WithId } from 'mongodb';
 
+import { auth } from 'express-oauth2-jwt-bearer';
+
+const jwtCheck = auth({
+  audience: 'https://hello-world.example.com',
+  issuerBaseURL: 'https://dev-8j9ru6to.us.auth0.com/',
+  tokenSigningAlg: 'RS256',
+  authRequired: false,
+});
+
 export default function (app: Router) {
   const route = Router();
   app.use('/ingredients', route);
 
-  route.get('/', async (_, res: Response<WithId<IIngredient>[]>) => {
+  route.get('/', jwtCheck, async (req, res: Response<WithId<IIngredient>[]>) => {
+    if (req.auth) {
+      console.log(req.auth.payload);
+    }
+    
     const ingredients = await getAllIngredients();
 
     res.status(200).json(ingredients);
